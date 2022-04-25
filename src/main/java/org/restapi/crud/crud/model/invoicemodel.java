62791ede11273cac0f2e1bbe1df8d3b584e3e6fe
Rajith Singh <rajithsingh2000@gmail.com>
2022-04-25 21:46:42 +0530

    Created calculation process of the invoice....

--- src/main/java/org/restapi/crud/crud/model/invoicemodel.java
+++ src/main/java/org/restapi/crud/crud/model/invoicemodel.java
@@ -2,6 +2,44 @@ package org.restapi.crud.crud.model;
 
 public class invoicemodel {
 	
+	private String invoice_id;
+	private String cus_nic;
+	private String month;
+	private double unit_calculation;	
+	public invoicemodel() {
+		
+	}
+	public invoicemodel(String invoice_id, String cus_nic, String month, double unit_calculation) {
+		super();
+		this.invoice_id = invoice_id;
+		this.cus_nic = cus_nic;
+		this.month = month;
+		this.unit_calculation = unit_calculation;
+	}
+	public String getInvoice_id() {
+		return invoice_id;
+	}
+	public void setInvoice_id(String invoice_id) {
+		this.invoice_id = invoice_id;
+	}
+	public String getCus_nic() {
+		return cus_nic;
+	}
+	public void setCus_nic(String cus_nic) {
+		this.cus_nic = cus_nic;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public double getUnit_calculation() {
+		return unit_calculation;
+	}
+	public void setUnit_calculation(double unit_calculation) {
+		this.unit_calculation = unit_calculation;
+	}
 
 
 }
--- src/main/java/org/restapi/crud/crud/resource/invoiceresource.java
+++ src/main/java/org/restapi/crud/crud/resource/invoiceresource.java
@@ -1,5 +1,58 @@
 package org.restapi.crud.crud.resource;
 
-public class invoiceresource {
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.restapi.crud.crud.model.invoicemodel;
+import org.restapi.crud.crud.service.invoiceservice;
+
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/crud")
+public class invoiceresource {
+	invoiceservice service = new invoiceservice();
+	
+	@Path("/invoice_insertion")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public invoicemodel addInvoice(invoicemodel invoice) {
+		return service.insertInvoice(invoice);
+	}
+	
+//	@Path("/unit_retrieve")
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public ArrayList<unitmodel> getUnit() throws SQLException {
+//		return service.getUnit();
+//	}
+//	
+//	@Path("/unit_retrieveById/{id}")
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public ArrayList<calcmodel> getUnits(@PathParam("id") int id) throws SQLException {
+//		return service.getUnitsById(id);
+//	}
+	
+//	@Path("/unit_retrieveById/{id}")
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public ArrayList<calcmodel> getUnits(@PathParam("id") int id) throws SQLException {
+//		return service.getUnitsById(id);
+//	}
+	
+//	@Path("/updateUser/{id}")
+//	@PUT
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	public unitmodel updateUser(crudmodel user) {
+//		return service.updateUser(user);
+//	}
 }
