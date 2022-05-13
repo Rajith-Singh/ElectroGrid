07a4b04fec8a78d7bfd893bcdd6bf84a2e7ccd27
Dulaksha <dulakshakavi@gmail.com>
2022-05-14 03:14:33 +0530

    updated

--- src/main/java/com/Model.java
+++ src/main/java/com/Model.java
@@ -15,6 +15,7 @@ public class Model {
 	 { 
 	 Class.forName("com.mysql.cj.jdbc.Driver"); 
 	 
+	 
 	 //Provide the correct details: DBServer/DBName, username, password 
 	 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogrid", "root",""); 
 	 } 
